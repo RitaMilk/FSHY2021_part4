e8ed560120febe587f57654d313758ad10cbd65f
RitaMilk <80791613+RitaMilk@users.noreply.github.com>
2021-05-09 18:38:50 +0300

    part 4.2
    different modules

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
-require('dotenv').config()
+//require('dotenv').config()
+const app=require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+//const express = require('express')
+//const app = express()
+//const cors = require('cors')
 
-const blogSchema = mongoose.Schema({
-  title: String, 
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 //const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl = process.env.MONGODB_URI
-console.log('connecting to', mongoUrl)
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+///const mongoUrl = process.env.MONGODB_URI
+//4.2console.log('connecting to', config.MONGODB_URI)
+/*4.2 logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) 4.2*/
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+//const PORT = 3003
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+  //console.log(`Server running on port ${config.PORT}`)
 })
